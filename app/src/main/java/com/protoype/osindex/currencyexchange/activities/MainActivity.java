5eb9622c40950f8aebff28f89ba3efb1490a36fd
Ifeanyi Osinakayah <o.ifeanyi@proteuxtechnologies.net>
2017-11-06 15:15:41 +0100

    Removed the default seeting on the option menu

--- app/src/main/java/com/protoype/osindex/currencyexchange/activities/MainActivity.java
+++ app/src/main/java/com/protoype/osindex/currencyexchange/activities/MainActivity.java
@@ -91,8 +91,7 @@ public class MainActivity extends AppCompatActivity implements CurrencyClickList
             @Override
             public void onClick(View view) {
                 new MaterialDialog.Builder(MainActivity.this)
-                            .title("New Currency")
-                            .content("Add new currency")
+                            .title("Add new currency")
                             .positiveText("Save")
                             .negativeText("Cancel")
                             .items(getUnAddedCurrencies())
@@ -110,9 +109,6 @@ public class MainActivity extends AppCompatActivity implements CurrencyClickList
                                 }
                             })
                             .show();
-
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
             }
         });
         initPermanentSnackBAr();
@@ -134,10 +130,7 @@ public class MainActivity extends AppCompatActivity implements CurrencyClickList
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        else if(id == R.id.menu_refresh){
+        if(id == R.id.menu_refresh){
             refreshExhangeRate();
         }
 
@@ -248,7 +241,7 @@ public class MainActivity extends AppCompatActivity implements CurrencyClickList
 
     private void initPermanentSnackBAr(){
         Snackbar snackbar = Snackbar
-                .make(recyclerViewCurrency, "Updated on "+Utility.getInstance(this).readSharedPref(), Snackbar.LENGTH_INDEFINITE)
+                .make(recyclerViewCurrency, "Updated on "+Utility.getInstance(this).readSharedPref(), Snackbar.LENGTH_LONG)
                 .setAction("Refresh", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
